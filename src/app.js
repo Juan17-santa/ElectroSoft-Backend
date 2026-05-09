ef096dc14e9dde3b6147e8b06aa3ed05e16723a4
Dirgua <zxienceproyect@gmail.com>
2026-05-09 15:04:06 -0500

    feat(clients): implementar módulo completo de clientes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import clientRoutes from "./modules/clients/infrastructure/ClientRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/clients", clientRoutes);
 
 export default app;
--- src/modules/clients/infrastructure/ClientModel.js
+++ src/modules/clients/infrastructure/ClientModel.js
@@ -1,1 +1,13 @@
-// ClientModel.js
+import mongoose from 'mongoose';
+
+const ClientSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    address: { type: String, required: true },
+    documentType: { type: String, required: true },
+    documentNumber: { type: String, required: true, unique: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+export const ClientModel = mongoose.model('clients', ClientSchema);
