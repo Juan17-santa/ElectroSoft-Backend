11c9a288bb665a4af66cae29ddcc31ebc56ba45e
deiviss2006 <deivissamuel2017@gmail.com>
2026-06-04 22:53:51 -0500

    chore(app): update application configuration

--- src/app.js
+++ src/app.js
@@ -18,7 +18,8 @@ import paymentRouter from "./modules/payments/infrastructure/PaymentRoutes.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use((req, _res, next) => {
     req.url = req.url.replace(/(%0A|%0D|\s)+$/gi, "");
     next();
