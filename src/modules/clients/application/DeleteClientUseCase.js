232d0667680e4587e5805507569735f3ed2c5a70
Dirgua <zxienceproyect@gmail.com>
2026-08-17 23:14:13 -0500

    feat: improve clients, users and sales functionalities

--- src/modules/clients/application/DeleteClientUseCase.js
+++ src/modules/clients/application/DeleteClientUseCase.js
@@ -1,12 +1,20 @@
 export default class DeleteClientUseCase {
-    constructor(clientRepository) {
+    constructor(clientRepository, saleRepository) {
         this.clientRepository = clientRepository;
+        this.saleRepository = saleRepository;
     }
 
     async execute(id) {
         const existing = await this.clientRepository.findById(id);
 
         if (!existing) throw new Error('Cliente no encontrado');
+
+        if (this.saleRepository) {
+            const hasSales = await this.saleRepository.hasSalesByClient(id);
+            if (hasSales) {
+                throw new Error('No se puede eliminar el cliente porque tiene ventas asociadas');
+            }
+        }
 
         return await this.clientRepository.delete(id);
     }
